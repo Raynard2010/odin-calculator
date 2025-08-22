27b9733f0ff838bf91a8bea7f23f98671fb1d8d2
Raynard Chandrawangsa <raynardchandrawangsa@gmail.com>
2025-08-22 21:59:30 +0700

    Made html calculator

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a, b){
+    return a + b;
+}
+
+let substract = function(a, b){
+    return a - b;
+}
+
+let multiply = () =>{
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
